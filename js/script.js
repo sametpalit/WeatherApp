a97e76d347df9378477fe2bf91f66db5d57b01ca
Samet Palit <sametpalit@live.nl>
2024-04-25 11:58:22 +0200

    snelle commit

--- js/script.js
+++ js/script.js
@@ -68,7 +68,7 @@ const getWeatherDetails = (cityName, lat, lon) => {
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim();
     if (!cityName) return;
-    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
 
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
         if (!data.length) return alert(`Geen coordinaten gevonden voor ${cityName}`);
